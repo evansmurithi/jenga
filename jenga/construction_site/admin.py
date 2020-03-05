68438a9cf1c885b41d95434193404e5df37ba9b7
Evans Murithi <murithievans80@gmail.com>
2020-03-05 07:28:51 +0300

    Alter list display for Delivery admin list page

--- jenga/construction_site/admin.py
+++ jenga/construction_site/admin.py
@@ -3,7 +3,13 @@ from django.contrib import admin
 from . import models
 
 
+class DeliveryAdmin(admin.ModelAdmin):
+    list_display = (
+        'supplier', 'site', 'material', 'quantity', 'delivered_on'
+    )
+
+
 admin.site.register(models.Supplier)
 admin.site.register(models.Material)
 admin.site.register(models.ConstructionSite)
-admin.site.register(models.Delivery)
+admin.site.register(models.Delivery, DeliveryAdmin)
